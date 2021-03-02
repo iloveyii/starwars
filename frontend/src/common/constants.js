19c72dbf1fc296358271e92bea4ddc80b50d8475
Hazrat Ali <iloveyii@yahoo.com>
2021-03-03 00:28:48 +0100

    port fix

--- frontend/src/common/constants.js
+++ frontend/src/common/constants.js
@@ -1,7 +1,6 @@
-const apiServer =
-  process.env.API_URL && process.env.API_PORT
-    ? process.env.API_URL + ":" + process.env.API_PORT
-    : "http://localhost:9000";
+const apiServer = process.env.API_URL
+  ? process.env.API_URL + ":" + process.env.API_PORT
+  : "http://localhost:9000";
 console.log("apiServer:", apiServer);
 
 export { apiServer };
